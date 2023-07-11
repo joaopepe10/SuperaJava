24e6db2ae19081ab45243d328efc8bdaa11992ab
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-11 13:15:37 -0300

    Adicionando um exception para validar a entrada de numeros negativos, foi criado uma nova excecao para fazer essa validacao, criei uma forma de adicionar os numeros pelo teclado

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import desafios.d1.ExceptionNegativeNumbers;
 import desafios.d1.OrderBy;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.logging.Logger;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean exit = false;
         List<Integer> automaticList = new ArrayList<>();
         List<Integer> manualList = new ArrayList<>();
         int opc = 0;
@@ -18,18 +20,33 @@ public class Main {
         System.out.println(orderBy.orderBy());
         
         //ADICIONANDO OS NUMEROS PELO TECLADO
-        try {
-            do {
-                System.out.println("======ORDENACAO DE NUMEROS=====" +
-                        "\n1-ADICIONAR NUMERO" +
-                        "\n2-SAIR");
-                opc = input.nextInt();
-                Integer x = input.nextInt();
-                manualList.add(x);
-            }while (opc != 2);
-        }catch ( )
+        while (!exit){
+            try {
+                    System.out.println("======ORDENACAO DE NUMEROS=====" +
+                            "\n1-ADICIONAR NUMERO" +
+                            "\n2-ORDENAR NUMEROS" +
+                            "\n3-SAIR");
+                    opc = input.nextInt();
 
-        
-        
+                    if(opc == 1){
+                        int x = input.nextInt();
+                        if (x < 0){
+                            throw new ExceptionNegativeNumbers();
+                        }
+                        manualList.add(x);
+                        System.out.println("Numero " + x + " adicionado com sucesso!");
+                    } else if (opc == 2) {
+                        OrderBy o = new OrderBy(manualList);
+                        System.out.println(o.orderBy());
+                    } else if (opc == 3) {
+                        exit = true;
+                        System.out.println("Saindo do programa, volte sempre!");
+                    }else {
+                        System.out.println("Opcao invalida, tente novamente!");
+                    }
+            }catch (ExceptionNegativeNumbers e){
+                System.out.println("Erro: " + e.getMessage());
+            }
+        }
     }
 }
---
+++ src/desafios/d1/ExceptionNegativeNumbers.java
@@ -0,0 +1,7 @@
+package desafios.d1;
+
+public class ExceptionNegativeNumbers extends Exception{
+    public ExceptionNegativeNumbers(){
+        super("Numeros negativos nao sao permitidos!");
+    }
+}
