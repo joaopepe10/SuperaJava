98b52c4c0983a8e8d37d36059b552a7fe470cf5d
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-11 12:15:07 -0300

    Criado a classe OrderBy para fazer a ordenacao dos numeros, criado o metodo de ordenacao, fiz a separacao dos pares e impares, agora irei implementar uma forma de deixar os pares em ordem crescete e os impares em ordem decrescente

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import desafios.d1.OrderBy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<Integer> l = new ArrayList<>();
+        l.addAll(Arrays.asList(87, 543, 10, 4));
+        OrderBy orderBy = new OrderBy(l);
+        System.out.println(orderBy.orderBy());
     }
 }
--- src/desafios/d1/OrderBy.java
+++ src/desafios/d1/OrderBy.java
@@ -1,4 +1,56 @@
 package desafios.d1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderBy {
+    private List<Integer> numbers = new ArrayList<>();
+    private List<Integer> pair = new ArrayList<>();
+    private List<Integer> odd = new ArrayList<>();
+    private List<Integer> finalList = new ArrayList<>();
+
+
+    public OrderBy(){
+    }
+
+    public OrderBy(List<Integer> n){
+        this.numbers = n;
+    }
+
+    public OrderBy(Integer i){
+        this.numbers.add(i);
+    }
+
+    public List<Integer> orderBy(){
+        for (Integer i: this.numbers){
+            if(i % 2 == 0){
+                this.pair.add(i);
+            }else if (i % 2 == 1){
+                this.odd.add(i);
+            }
+        }
+        this.finalList.addAll(this.pair);
+        this.finalList.addAll(this.odd);
+        return this.finalList;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public List<Integer> getPair() {
+        return pair;
+    }
+
+    public void setPair(List<Integer> pair) {
+        this.pair = pair;
+    }
+
+    public List<Integer> getOdd() {
+        return odd;
+    }
+
+    public void setOdd(List<Integer> odd) {
+        this.odd = odd;
+    }
 }
