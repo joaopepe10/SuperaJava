66f9afcd6c7a09a644b9ed7f4acf084ac5f234f0
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-11 18:20:41 -0300

    Estava fazendo alguns testes e precisei arredondar o value para duas casas decimais utilizando a classe Match

--- src/desafios/d2/Main.java
+++ src/desafios/d2/Main.java
@@ -1,8 +1,22 @@
 package desafios.d2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         MinGrades m = new MinGrades();
-        m.calculateGradesCoins();
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Digite um valor");
+            Double value = in.nextDouble();
+            if (value < 0){
+                throw new ArithmeticException();
+            }
+            m.calculateGradesCoins(value);
+        }catch (ArithmeticException | InputMismatchException e){
+            System.out.println("Erro, valor invalido!");
+        }
+
     }
 }
--- src/desafios/d2/MinGrades.java
+++ src/desafios/d2/MinGrades.java
@@ -50,4 +50,42 @@ public class MinGrades {
         System.out.println("\tQuantidade de moedas de R$0.01: " + countCoins[5]);
 
     }
+    public void calculateGradesCoins(Double value){
+        //CALCULAR QUANTIDADE DE NOTAS
+        for (int i = 0; i < grades.length; ++i){
+            //VERIFICACAO PARA EVITAR ERROS AO LONGO DA APLICACAO, POIS SE O VALUE E MAIOR QUE AS NOTAS ELE PODE FAZER A DIVISAO E SABER A QUANTIDADE NECESSARIA DE NOTAS USADAS
+            if (value > grades[i]){
+                //CASTING PARA ATRIBUIR A QUANTIDADE DE NOTAS NECESSARIAS DE ACORDO COM SEU INDICE
+                double x = value / grades[i];
+                countGrades[i] = (int)x;
+                //ATUALIZACAO DO VALOR DE VALUE PARA A PROXIMA ITERACAO DO FOR
+                value = value - (countGrades[i] * grades[i]);
+            }
+        }
+        //CALCULANDO QUANTIDADE DE MOEDAS USADAS
+        for(int i = 0; i < coins.length; ++i){
+            if (value > coins[i]){
+                value = Math.round(value * 100.0) / 100.0;
+                double result = value / coins[i];
+                //CASTING PARA OBTER O VALOR INTEIRO DA QUANTIDADE DE MOEDAS DE DETERMINADO TIPO DE ACORDO COM O INDICE
+                countCoins[i] = (int) result;
+                value = value - (countCoins[i] * coins[i]);
+            }
+        }
+        System.out.println("Notas: ");
+        System.out.println("\tQuantidade de notas de R$100: " + countGrades[0]);
+        System.out.println("\tQuantidade de notas de R$50: " + countGrades[1]);
+        System.out.println("\tQuantidade de notas de R$20: " + countGrades[2]);
+        System.out.println("\tQuantidade de notas de R$10: " + countGrades[3]);
+        System.out.println("\tQuantidade de notas de R$5: " + countGrades[4]);
+        System.out.println("\tQuantidade de notas de R$2: " + countGrades[5]);
+        System.out.println("Moedas: ");
+        System.out.println("\tQuantidade de moedas de R$1.00: " + countCoins[0]);
+        System.out.println("\tQuantidade de moedas de R$0.50: " + countCoins[1]);
+        System.out.println("\tQuantidade de moedas de R$0.25: " + countCoins[2]);
+        System.out.println("\tQuantidade de moedas de R$0.10: " + countCoins[3]);
+        System.out.println("\tQuantidade de moedas de R$0.05: " + countCoins[4]);
+        System.out.println("\tQuantidade de moedas de R$0.01: " + countCoins[5]);
+
+    }
 }
