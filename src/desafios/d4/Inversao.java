f437f6485a5d4def19e5fd9f77c802e22ce06a02
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-14 00:47:50 -0300

    Ja consigo fazer a manipulacao da string, estou usando char pois acho mais pratico

--- src/desafios/d4/Inversao.java
+++ src/desafios/d4/Inversao.java
@@ -14,6 +14,18 @@ public class Inversao {
         int tamTotal = getTexto().length();
         String esq = texto.substring(0,11);
         String dir = texto.substring(11);
+        char[] novaEsq = new char[tamTotal / 2];
+        for (int i = 0, j = tamTotal / 2 - 1;i < tamTotal / 2; ++i, --j){
+            if (i == 5 || j == 5){
+                novaEsq[i] = esq.charAt(j);
+                continue;
+            }
+            novaEsq[i] = esq.charAt(j);
+
+        }
+        for (int i = 0; i<11;i++){
+            System.out.print(novaEsq[i]);
+        }
 
     }
 
