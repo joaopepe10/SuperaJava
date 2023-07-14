4664a5b2de77208272f1a22690ced0e1e296ef1d
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-13 23:49:16 -0300

    A ideia e criar uma forma de verificar o tamanho de caracter que tem na linha, dividir em dois, e fazer a inversao de cada caracter para seu respectivo local, ja que isso segue um padrao, exemplo o caracter na posicao 0 vai trocar com a posicao 10 e assim ate chegar no meio da coluna, onde o meio nao ira alterar de posicao

---
+++ src/desafios/d4/Inversao.java
@@ -0,0 +1,25 @@
+package desafios.d4;
+
+public class Inversao {
+    private String texto = "this line is gibberish";
+
+    public Inversao() {
+    }
+
+    public Inversao(String texto) {
+        this.texto = texto;
+    }
+
+    public void inversaoString(){
+        int tamTotal = getTexto().length();
+        texto.
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
---
+++ src/desafios/d4/Main.java
@@ -0,0 +1,8 @@
+package desafios.d4;
+
+public class Main {
+    public static void main(String[] args) {
+        Inversao inversao = new Inversao();
+        inversao.inversaoString();
+    }
+}
