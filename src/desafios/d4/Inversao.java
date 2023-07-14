a5c34a24345b36ae04b989f9c3c92b531770b941
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-14 01:10:43 -0300

    Implementacao feita para as duas colunas, tanto para esquerda, quanto para direita

--- src/desafios/d4/Inversao.java
+++ src/desafios/d4/Inversao.java
@@ -2,38 +2,65 @@ package desafios.d4;
 
 public class Inversao {
     private String texto = "this line is gibberish";
+    private String textoInvertido = "I ENIL SIHTHSIREBBIG S";
+    private String[] linhas = new String[5];
 
     public Inversao() {
+        linhas[0] = "I ENIL SIHTHSIREBBIG S";
+        linhas[1] = "LEVELKAYAK";
+        linhas[2] = "H YPPAHSYADILO";
+        linhas[3] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        linhas[4] = "VOD OWT SNEH HCNERF EGDIRTRAP A DNA SE";
     }
 
-    public Inversao(String texto) {
-        this.texto = texto;
-    }
 
     public void inversaoString(){
-        int tamTotal = getTexto().length();
-        String esq = texto.substring(0,11);
-        String dir = texto.substring(11);
+        int tamTotal = getTextoInvertido().length();
+        String esq = textoInvertido.substring(0,11);
+        String dir = textoInvertido.substring(11);
         char[] novaEsq = new char[tamTotal / 2];
-        for (int i = 0, j = tamTotal / 2 - 1;i < tamTotal / 2; ++i, --j){
-            if (i == 5 || j == 5){
+        char[] novaDir = new char[tamTotal / 2];
+
+        for (int i = 0, j = tamTotal / 2 - 1; i < tamTotal / 2; ++i, --j){
+            if (i == (tamTotal / 2) / 2 || j == (tamTotal / 2) / 2){
                 novaEsq[i] = esq.charAt(j);
                 continue;
             }
             novaEsq[i] = esq.charAt(j);
-
         }
+
+        for (int i = 0, j = tamTotal / 2 - 1; i < tamTotal / 2; ++i, --j){
+            if (i == (tamTotal / 2) / 2 || j == (tamTotal / 2) / 2){
+                novaDir[i] = dir.charAt(j);
+                continue;
+            }
+            novaDir[i] = dir.charAt(j);
+        }
+
         for (int i = 0; i<11;i++){
             System.out.print(novaEsq[i]);
         }
+        System.out.print("");
+
+        for (int i = 0; i<11;i++){
+            System.out.print(novaDir[i]);
+        }
 
     }
 
-    public String getTexto() {
-        return texto;
+    public String getTextoInvertido() {
+        return textoInvertido;
     }
 
-    public void setTexto(String texto) {
-        this.texto = texto;
+    public void setTextoInvertido(String textoInvertido) {
+        this.textoInvertido = textoInvertido;
+    }
+
+    public String[] getLinhas() {
+        return linhas;
+    }
+
+    public void setLinhas(String[] linhas) {
+        this.linhas = linhas;
     }
 }
